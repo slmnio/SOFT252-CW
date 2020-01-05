bc16a6e0948a0de4a3a49e04b9ce18446345db10
Solomon Cammack <solomon@solomon.eu.com>
2020-01-05 19:08:00 +0000

    Add User base abstract structure

--- PatientManagement/src/main/java/com/slmn/patientmanagement/structures/User.java
+++ PatientManagement/src/main/java/com/slmn/patientmanagement/structures/User.java
@@ -3,3 +3,18 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package com.slmn.patientmanagement.structures;
+
+/**
+ *
+ * @author solca
+ */
+public abstract class User {
+    private String ID;
+    
+    private String givenName;
+    private String surname;
+    private String address;
+    
+    // age and gender aren't required for all users
+}
